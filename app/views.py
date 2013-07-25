eab530fde83d3e179a248169b20d47783a4d8faa
amyrlam <amy.r.lam@gmail.com>
2013-07-24 20:51:40 -0700

    end of flask mega part iii

--- app/views.py
+++ app/views.py
@@ -25,8 +25,9 @@ def index():
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Login requested for OpenID="' + form.openid.data + str(form.remember_me.data))
+		flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
 		return redirect('/index')
 	return render_template('login.html',
 		title = 'Sign In',
-		form = form)
+		form = form,
+		providers = app.config['OPENID_PROVIDERS'])
