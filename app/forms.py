7467219b3473b9d31de5aa3b75d8baac0fa5ebc2
amyrlam <amy.r.lam@gmail.com>
2013-07-30 15:56:56 -0700

    postgres success

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form, TextField, BooleanField, TextAreaField
 from flask.ext.wtf import Required, Length
-from app.models import User
+from app.models import Users
 
 class LoginForm(Form):
 	openid = TextField('openid', validators = [Required()])
--- app/models.py
+++ app/models.py
@@ -7,11 +7,12 @@ ROLE_USER = 0
 ROLE_ADMIN = 1
 
 followers = db.Table('followers',
-	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+	db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
 )
 
-class User(db.Model):
+# "user" is a reserved word in postgres so changed table name from User to Users
+class Users(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	nickname = db.Column(db.String(64), unique = True)
 	email = db.Column(db.String(120), index = True, unique = True)
@@ -19,7 +20,7 @@ class User(db.Model):
 	posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime)
-	followed = db.relationship('User',
+	followed = db.relationship('Users',
 		secondary = followers,
 		primaryjoin = (followers.c.follower_id == id),
 		secondaryjoin = (followers.c.followed_id ==id),
@@ -28,12 +29,12 @@ class User(db.Model):
 
 	@staticmethod
 	def make_unique_nickname(nickname):
-		if User.query.filter_by(nickname = nickname).first() == None:
+		if Users.query.filter_by(nickname = nickname).first() == None:
 			return nickname
 		version = 2
 		while True:
 			new_nickname = nickname + str(version)
-			if User.query.filter_by(nickname = new_nickname).first() == None:
+			if Users.query.filter_by(nickname = new_nickname).first() == None:
 				break
 			version += 1
 		return new_nickname
@@ -53,24 +54,24 @@ class User(db.Model):
 	def avatar(self, size):
 		return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
 
-	def follow(self, user):
-		if not self.is_following(user):
-			self.followed.append(user)
+	def follow(self, users):
+		if not self.is_following(users):
+			self.followed.append(users)
 			return self
 
-	def unfollow(self, user):
-		if self.is_following(user):
-			self.followed.remove(user)
+	def unfollow(self, users):
+		if self.is_following(users):
+			self.followed.remove(users)
 			return self
 
-	def is_following(self, user):
-		return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+	def is_following(self, users):
+		return self.followed.filter(followers.c.followed_id == users.id).count() > 0
 
 	def followed_posts(self):
 		return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
 
 	def __repr__(self):
-		return '<User %r>' % (self.nickname)				
+		return '<Users %r>' % (self.nickname)				
 
 class Post(db.Model):
 	__searchable__ = ['body']
@@ -78,7 +79,7 @@ class Post(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	body = db.Column(db.String(140))
 	timestamp = db.Column(db.DateTime)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 	def __repr__(self):
 		return '<Post %r>' % (self.body)
