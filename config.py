79da672833b8bc258ce19d2d1bc43a9c12acb260
amyrlam <amy.r.lam@gmail.com>
2013-08-19 23:17:55 -0700

    more frontend

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 OPENID_PROVIDERS = [
-	{ 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-	{ 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+	{ 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' }]
+	#{ 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://amyrlam@localhost/earthmd_db'
 
